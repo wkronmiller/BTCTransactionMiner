f54f24fe04259192b33aba7aa3582b5702f41f67
Ubuntu <ubuntu@ip-172-30-3-92.ec2.internal>
2017-04-27 17:47:30 +0000

    Use relative paths

--- submit.sh
+++ submit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-spark-submit --class "coffee.rory.Main" --master local[*] --driver-memory 5g --executor-memory 6g /root/BTCTransactionMiner/target/scala-2.11/Bitcoin-Parser-assembly-0.0.1.jar
+spark-submit --class "coffee.rory.Main" --master local[*] --driver-memory 5g --executor-memory 6g target/scala-2.11/Bitcoin-Parser-assembly-0.0.1.jar
